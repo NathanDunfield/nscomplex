e681b957000d5a891c83d48d240d1655d5463f17
chaerynlee <chaeryn2@illinois.edu>
2022-10-06 12:45:02 -0500

    removed temporary files

--- build/lib/nscomplex/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import surfaces, regina_util, skeleta, connected_surfaces
-from .connected_surfaces import ConnectedSurfaces, MoreSurfacesNeedToFindFaces
-from .rehydrate import reconstruct_faces
-
